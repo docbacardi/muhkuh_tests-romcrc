a7d4bce0d140b760aee16415a6013949c3bcfc50
doc_bacardi <devnull@localhost>
2014-03-02 14:55:10 +0100

    Show the parameter and the calculated CRC. Return the calculated CRC.

--- crctest/src/main_test.c
+++ crctest/src/main_test.c
@@ -16,6 +16,7 @@ TEST_RESULT_T test(TEST_PARAMETER_T *ptTestParam)
 	CRCTEST_PARAMETER_T *ptTestParams;
 	const unsigned char *pucCnt;
 	const unsigned char *pucEnd;
+	unsigned long ulCrc32;
 
 
 	systime_init();
@@ -29,16 +30,22 @@ TEST_RESULT_T test(TEST_PARAMETER_T *ptTestParam)
 	/* Get the test parameter. */
 	ptTestParams = (CRCTEST_PARAMETER_T*)(ptTestParam->pvInitParams);
 
-	/* Build the CRC for */
+	/* Build the CRC for the requested area. */
 	pucCnt = ptTestParams->pucAreaStart;
 	pucEnd = ptTestParams->pucAreaEnd;
+
+	uprintf(". area start: 0x%08x\n", ptTestParams->pucAreaStart);
+	uprintf(". area end  : 0x%08x\n\n", ptTestParams->pucAreaEnd);
 	
+	uprintf("Calculating CRC32 ...\n");
 	crc_init_crc32();
 	while( pucCnt<pucEnd )
 	{
 		crc_update( *(pucCnt++) );
 	}
-	ptTestParams->ulCrc32 = crc_get_crc32();
+	ulCrc32 = crc_get_crc32();
+	ptTestParams->ulCrc32 = ulCrc32;
+	uprintf("CRC32 = 0x%08x\n", ulCrc32);
 
 	rdy_run_setLEDs(RDYRUN_GREEN);
 	tTestResult = TEST_RESULT_OK;
